8566ab29b0a1074964c10a25da827a8f43915cf9
Amol Gaikwad <amol.gaikwad@publicissapient.com>
2022-11-22 23:46:34 +0000

    Restructure

--- api-gateway/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = () => ({
-  HTTP_PORT: Number(process.env.HTTP_PORT),
-  AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL,
-  ORDER_SERVICE_URL: process.env.ORDER_SERVICE_URL,
-  PRODUCT_SERVICE_URL: process.env.PRODUCT_SERVICE_URL,
-});
